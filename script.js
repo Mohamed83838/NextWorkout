e3b1f2383f1ac2e801fe3bb33f8d877c90543460
blockytable <hosnifraj50@gmail.com>
2025-03-22 23:23:02 +0100

    refactor: improved script

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const MODE_NOT_STARTED = "notstarted";
 const MODE_STARTED = "started";
-const MODE_CONTINUED = "continued";
-const MODE_PAUSED = "paused";
-const MODE_CONTINUE = "continue";
 
 let currentMode = MODE_NOT_STARTED;
 let currentTime = 0;
@@ -26,16 +22,12 @@ function beep() {
 	audio.play();
 }
 
-function continueTimer() {
-	start();
-}
-
 function start() {
 	const timer = document.querySelector("#timer");
 	const time = document.querySelector("#time");
 	const start = document.querySelector("#start-btn");
 
-	if (currentMode == MODE_NOT_STARTED || currentMode == MODE_CONTINUED) {
+	if (currentMode == MODE_NOT_STARTED) {
 
 		currentMode = MODE_STARTED;
 		start.innerHTML = "Pause"
@@ -43,8 +35,8 @@ function start() {
 		beep();
 		if (currentTime == 0) {
 			currentTime = Number(time.value) + 1;
-			
-		} else console.log("timer is continued!!")
+			console.log("Timer started")
+		} else console.log("Timer continued")
 		interval = setInterval(function() {
 			if (currentTime > 0) {
 				currentTime -= 1;
@@ -53,17 +45,14 @@ function start() {
 				clearInterval(interval);
 				currentMode = MODE_NOT_STARTED;
 				start.innerHTML = "Start"
+				console.log("Timer finished")
 				beep();
 			}
 		}, 1000);
-	} else if (currentMode == MODE_CONTINUE) {
-		currentMode = MODE_CONTINUED;
-		console.log("Clicked continue, timer should be continued now!")
-		continueTimer();
 	} else {
 		start.innerHTML = "Continue"
-		console.log("Clicked Pause, now click continue")
-		currentMode = MODE_CONTINUE
+		console.log("Pause state")
+		currentMode = MODE_NOT_STARTED
 		clearInterval(interval);
 	}
 }
