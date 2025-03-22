9628f7b6b635cdc1cd28a554f12a391750d8ccfe
blockytable <hosnifraj50@gmail.com>
2025-03-22 22:32:33 +0100

    feat: pause/continue the timer

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-function start() {
+currentMode = "notstarted";
+
+function start(x) {
 	const timer = document.querySelector("#timer");
 	const time = document.querySelector("#time");
+	const start = document.querySelector("#start-btn");
+	if (currentMode == "notstarted" || currentMode == "continued"){
+		currentMode = "started";
+		start.innerHTML = "Pause"
 
 	beep();
 
-	currentTime = Number(time.value) + 1;
+	
+	if (x){
+		currentTime = x
+	} else {
+		currentTime = Number(time.value) + 1;
+	}
 	interval = setInterval(function(){
 		if (currentTime > 0) {
 			currentTime -= 1;
 			timer.innerHTML = formatTime(currentTime);
 		} else {
 			clearInterval(interval);
+			currentMode = "notstarted";
+			start.innerHTML = "Start"
 			beep();
 		}
 	}, 1000);
-}
+} else if (currentMode == "continue"){
+	currentMode = "continued";
+	start1(currentTime);
+	
+} else {
 
+	start.innerHTML = "Continue"
+	currentMode = "continue"
+	clearInterval(interval);
+}
+	
+}
+function start1(currentTime) {
+	// body...
+	start(currentTime);
+}
 function formatTime(time) {
 	mins = Math.floor(time / 60);
 	secs = time - (mins * 60);
