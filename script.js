f0ff88138d1ec1aec10c4b49b47d0a224358c9ee
blockytable <hosnifraj50@gmail.com>
2025-03-22 23:07:25 +0100

    refactor: improved code

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
-currentMode = "notstarted";
+const MODE_NOT_STARTED = "notstarted";
+const MODE_STARTED = "started";
+const MODE_CONTINUED = "continued";
+const MODE_PAUSED = "paused";
+const MODE_CONTINUE = "continue";
 
-function start(x) {
-	const timer = document.querySelector("#timer");
-	const time = document.querySelector("#time");
-	const start = document.querySelector("#start-btn");
-	if (currentMode == "notstarted" || currentMode == "continued"){
-		currentMode = "started";
-		start.innerHTML = "Pause"
+let currentMode = MODE_NOT_STARTED;
+let currentTime = 0;
+let interval;
 
-	beep();
-
-	
-	if (x){
-		currentTime = x
-	} else {
-		currentTime = Number(time.value) + 1;
-	}
-	interval = setInterval(function(){
-		if (currentTime > 0) {
-			currentTime -= 1;
-			timer.innerHTML = formatTime(currentTime);
-		} else {
-			clearInterval(interval);
-			currentMode = "notstarted";
-			start.innerHTML = "Start"
-			beep();
-		}
-	}, 1000);
-} else if (currentMode == "continue"){
-	currentMode = "continued";
-	start1(currentTime);
-	
-} else {
-
-	start.innerHTML = "Continue"
-	currentMode = "continue"
-	clearInterval(interval);
-}
-	
-}
-function start1(currentTime) {
-	// body...
-	start(currentTime);
-}
 function formatTime(time) {
 	mins = Math.floor(time / 60);
 	secs = time - (mins * 60);
@@ -56,6 +21,48 @@ function formatTime(time) {
 }
 
 function beep() {
-    const audio = new Audio('media/beep.mp3'); 
-    audio.play();
+	const audio = new Audio('media/beep.mp3');
+	audio.play();
+}
+
+function continueTimer() {
+	start();
+}
+
+function start() {
+	const timer = document.querySelector("#timer");
+	const time = document.querySelector("#time");
+	const start = document.querySelector("#start-btn");
+
+	if (currentMode == MODE_NOT_STARTED || currentMode == MODE_CONTINUED) {
+
+		currentMode = MODE_STARTED;
+		start.innerHTML = "Pause"
+
+		beep();
+		if (currentTime == 0) {
+			currentTime = Number(time.value) + 1;
+			
+		} else console.log("timer is continued!!")
+		interval = setInterval(function() {
+			if (currentTime > 0) {
+				currentTime -= 1;
+				timer.innerHTML = formatTime(currentTime);
+			} else {
+				clearInterval(interval);
+				currentMode = MODE_NOT_STARTED;
+				start.innerHTML = "Start"
+				beep();
+			}
+		}, 1000);
+	} else if (currentMode == MODE_CONTINUE) {
+		currentMode = MODE_CONTINUED;
+		console.log("Clicked continue, timer should be continued now!")
+		continueTimer();
+	} else {
+		start.innerHTML = "Continue"
+		console.log("Clicked Pause, now click continue")
+		currentMode = MODE_CONTINUE
+		clearInterval(interval);
+	}
 }
