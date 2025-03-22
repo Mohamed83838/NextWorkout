80df8f1de967c703e4d9bcd8bf5ddef32efa881b
blockytable <hosnifraj50@gmail.com>
2025-03-22 21:47:08 +0100

    feat: time formating, play sound at the end of timers

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ function start() {
 	const timer = document.querySelector("#timer");
 	const time = document.querySelector("#time");
 
-	currentTime = Number(time.value);
+	beep();
+
+	currentTime = Number(time.value) + 1;
 	interval = setInterval(function(){
 		if (currentTime > 0) {
 			currentTime -= 1;
-			timer.innerHTML = currentTime;
+			timer.innerHTML = formatTime(currentTime);
 		} else {
 			clearInterval(interval);
+			beep();
 		}
 	}, 1000);
+}
+
+function formatTime(time) {
+	mins = Math.floor(time / 60);
+	secs = time - (mins * 60);
+	if (secs < 10) {
+		secs = "0" + secs
+	}
+	if (mins < 10) {
+		mins = "0" + mins
+	}
+	return mins + ":" + secs;
+}
+
+function beep() {
+    const audio = new Audio('media/beep.mp3'); 
+    audio.play();
 }
