202f0d0d92e1bf088a5354e617cd62c87ee646ba
mohammed <mohamedfraj610@gmail.com>
2025-03-22 21:59:12 -0800

    ho

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function start() {
+	console.log("start")
 	timer = document.querySelector("#timer")
 	time = Number(document.querySelector("#time").value)
 	x = time
 	x = setInterval(function() {
-		if (x > time):
+		if (x > 0){
 			x -= 1
 			timer.innerHTML = x
+		}
 	}, 1000);
 	
 }
